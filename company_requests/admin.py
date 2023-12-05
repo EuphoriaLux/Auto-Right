d0da9108d4259d77b9e6ea772296ce08955d08b3
tom <tom.scheuer1993@gmail.com>
2023-12-05 01:57:46 +0100

    Update for upload database fixture

--- company_requests/admin.py
+++ company_requests/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
 from .models import AuthorizationScope, Category, CompanyGroup, Company, CompanyRequest, Risk, CategoryRisk, UserAuthorization
+from django import forms
+import json
+from django.contrib import messages
+from django.shortcuts import render
+
+class JSONUploadForm(forms.Form):
+    json_file = forms.FileField()
+
+
+@admin.action(description='Upload JSON data')
+def upload_json_data(modeladmin, request, queryset):
+    if request.method == 'POST':
+        form = JSONUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            json_file = request.FILES['json_file']
+            data = json.load(json_file)
+
+            for entry in data:
+                model_name = entry['model']
+                pk = entry['pk']
+                fields = entry['fields']
+
+                if model_name == "company_requests.risk":
+                    category = CategoryRisk.objects.get(pk=fields['category']) if fields['category'] else None
+                    Risk.objects.update_or_create(
+                        pk=pk, defaults={**fields, 'category': category}
+                    )
+
+                elif model_name == "company_requests.categoryrisk":
+                    CategoryRisk.objects.update_or_create(
+                        pk=pk, defaults=fields
+                    )
+
+                # Add similar blocks for other models (Category, CompanyGroup, Company, etc.)
+
+            messages.success(request, "JSON data has been successfully processed.")
+            return HttpResponseRedirect(reverse('admin:index'))
+    else:
+        form = JSONUploadForm()
+
+    return render(request, 'admin/json_upload.html', {'form': form})
+
 
 # Inline for CategoryRisk -> Risk
 class RiskInline(admin.TabularInline):
@@ -48,6 +90,8 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description']
     list_filter = ['name']
     filter_horizontal = ('risk',)
+    actions = [upload_json_data]  # Add this line
+
 
 # CompanyGroup Admin
 class CompanyGroupAdmin(admin.ModelAdmin):
@@ -69,6 +113,8 @@ class CompanyRequestAdmin(admin.ModelAdmin):
     list_filter = ['request_date', 'company', 'status', 'request_type']
     list_editable = ['status']
 
+
+
 admin.site.register(Risk, RiskAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(CompanyGroup, CompanyGroupAdmin)
@@ -77,3 +123,4 @@ admin.site.register(CompanyRequest, CompanyRequestAdmin)
 admin.site.register(CategoryRisk, CategoryRiskAdmin)
 admin.site.register(AuthorizationScope, AuthorizationScopeAdmin)
 admin.site.register(UserAuthorization, UserAuthorizationAdmin)
+
